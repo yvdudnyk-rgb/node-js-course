0a51e3e0397a9ecb90d54dee658fae965e3261e5
Evgeny_Dudnick <y.v.dudnyk@student.khai.edu>
2026-04-02 14:29:34 +0300

    fix-setup-db

--- src/error.middleware.js
+++ src/error.middleware.js
@@ -1,6 +1,5 @@
 const errorResponder = (err, request, response, next) => {
-    response.header("Content-Type", 'application/json')
-    response.status(err.statusCode).send(err.message)
+   response.header("Content-Type", 'application/json')
+   response.status(err.statusCode).send(err.message)
 }
 module.exports = { errorResponder };
-
--- src/middleware.js
+++ src/middleware.js
@@ -2,16 +2,16 @@
 
 // A simple middleware function that logs the request details
 function logRequest(req, res, next) {
-    console.log(`Received a ${req.method} request to ${req.url}`);
+   console.log(`Received a ${req.method} request to ${req.url}`);
    next(); // Call the next middleware function in the chain
 }
 
 function blockSpecialBrand(req, res, next) {
-    if (req.params.brand === 'Brand C') {
-        res.status(403).send('Unavailable Brand');
-    } else {
-        next();
-    }
+   if (req.params.brand === 'Brand C') {
+       res.status(403).send('Unavailable Brand');
+   } else {
+       next();
+   }
 }
 
 module.exports = { logRequest, blockSpecialBrand };
